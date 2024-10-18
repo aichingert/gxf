aed9dc00b8822a415ccc44891da8a7dd94b9d018
aichingert <aichinger.tobias13@gmail.com>
2024-10-18 21:59:06 +0200

    chore: added next steps

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -13,6 +13,7 @@ type parser struct {
 
     code uint16
     line string
+
 }
 
 func ParseBuffer(buffer []byte) (*drawing.Gxf, error) {
@@ -20,6 +21,32 @@ func ParseBuffer(buffer []byte) (*drawing.Gxf, error) {
     impl.bytes = buffer
 
     p := newParser(impl)
+
+
+    // TODO: next step is block parsing.
+
+    // Idea 1:
+    // A temporary solution would be to put the blocks into the parser and just calculate the positions
+    // when parsing and put the values directly in the buffer again and not use the block reference.
+
+    // Idea 2:
+    // Using instancing with the gpu to not store to not make the buffers too large. Have to research
+    // if it is costly to instance many different meshes because most of the times there are many blocks.
+
+    // OPTIONAL: using mesh optimizing algorithms to reduce size
+
+    // TODO: after that implement hatch entity
+
+    // figure out how to connect triangles and consider actually using indices
+
+    // TODO: circle smoothness
+
+    // Idea 1:
+    // create a parse config struct to specify how smooth corners of a circle should be and polylines with
+    // a bulge as well
+
+    // OPTIONAL: just predefine it.
+
     gxf := drawing.NewGxf()
 
     return p.parse(gxf)
